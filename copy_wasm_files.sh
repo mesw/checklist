23a56be343f1398b1771bcdce6fb52616e3c0078
MESW <mesw@mailbox.org>
2026-03-08 00:28:17 +0100

    build wasm

---
+++ copy_wasm_files.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cp build/WebAssembly_Qt_6_8_3_single_threaded-Release/checklist.* wasm/ -v
+cp build/WebAssembly_Qt_6_8_3_single_threaded-Release/qtloader.js wasm/ -v
+cp build/WebAssembly_Qt_6_8_3_single_threaded-Release/qtlogo.svg wasm/ -v
+
+
